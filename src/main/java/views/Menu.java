4d94fa40bb5b4d036d3899a5a40aa9cccdf0fc95
Matias <mollo.matias03@gmail.com>
2025-06-04 17:04:00 -0300

    Vista clientes disponibles

--- src/main/java/views/Menu.java
+++ src/main/java/views/Menu.java
@@ -9,17 +9,20 @@ public class Menu extends JFrame {
         this.setTitle("Menu - Gestor de tarjetas");
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(2,1));
+        setLayout(new GridLayout(3,1));
         setSize(500,400);
 
         JButton agregarTarjeta = new JButton("Agregar Tarjeta");
         JButton agregarCliente = new JButton("Agregar Cliente");
+        JButton listarClientes = new JButton("Listar Clientes");
 
         agregarTarjeta.addActionListener(e -> SwingUtilities.invokeLater(AgregarTarjeta::new));
         agregarCliente.addActionListener(e -> SwingUtilities.invokeLater(AgregarCliente::new));
+        listarClientes.addActionListener(e -> SwingUtilities.invokeLater(ClientesDisponibles::new));
 
         add(agregarTarjeta);
         add(agregarCliente);
+        add(listarClientes);
 
         setVisible(true);
     }
